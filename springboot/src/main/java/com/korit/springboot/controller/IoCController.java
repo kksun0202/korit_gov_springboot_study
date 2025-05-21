651883f49f7f3a785a4a88e8285b86f706ba47de
kksun0202 <kksun0202@naver.com>
2025-05-21 16:53:06 +0900

    20250521 IoC / Rest API ê

--- springboot/src/main/java/com/korit/springboot/controller/IoCController.java
+++ springboot/src/main/java/com/korit/springboot/controller/IoCController.java
@@ -1,6 +1,7 @@
 package com.korit.springboot.controller;
 
 
+import com.korit.springboot.ioc.BeanStudy;
 import com.korit.springboot.ioc.스마트폰;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ public class IoCController {
 
     @Autowired
     private 스마트폰 s;
+    private BeanStudy beanStudy;
 
     @GetMapping("/ioc")
     public String get() {
---
+++ springboot/src/main/java/com/korit/springboot/ioc/BeanStudy.java
@@ -0,0 +1,8 @@
+package com.korit.springboot.ioc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanStudy {
+
+}

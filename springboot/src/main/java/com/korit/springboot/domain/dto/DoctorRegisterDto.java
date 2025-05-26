ad97d417fb8e93a53a3cab976e13650b755c36ed
kksun0202 <kksun0202@naver.com>
2025-05-26 17:52:51 +0900

    20250526 JPA 기초

---
+++ springboot/src/main/java/com/korit/springboot/domain/dto/DoctorRegisterDto.java
@@ -0,0 +1,17 @@
+package com.korit.springboot.domain.dto;
+
+import com.korit.springboot.domain.entity.DoctorEntity;
+import lombok.Data;
+
+@Data
+public class DoctorRegisterDto {
+    private String doctorName;
+    private String departmentName;
+
+    public DoctorEntity toEntity() {
+        return DoctorEntity.builder()
+                .doctorName(doctorName)
+
+                .build();
+    }
+}
